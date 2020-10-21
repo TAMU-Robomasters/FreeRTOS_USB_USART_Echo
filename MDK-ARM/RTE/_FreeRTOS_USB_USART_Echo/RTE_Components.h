5a81854027a3820ed0e8b9ef754e7662783e2a3a
wilsonwang881 <mitfans881@gmail.com>
2020-10-21 14:49:02 -0500

    initial commit

---
+++ MDK-ARM/RTE/_FreeRTOS_USB_USART_Echo/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FreeRTOS_USB_USART_Echo' 
+ * Target:  'FreeRTOS_USB_USART_Echo' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
